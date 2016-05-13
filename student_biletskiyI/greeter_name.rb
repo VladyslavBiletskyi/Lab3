5088f93f2b9bd1c787822646775c6c62b8a70520
ihorbiletskyi <ihor.biletskyi@nure.ua>
2016-05-13 12:47:57 +0300

    add my folder

---
+++ student_biletskiyI/greeter_name.rb
@@ -0,0 +1,9 @@
+class Greeter
+	def initialize(who)
+		@who = who
+	end
+	def greet
+		"Hello, #{@who tout you how to hate \m/}"
+		// no comments
+	end
+end
---
+++ student_biletskiyI/hello_biletskyiI.rb
@@ -0,0 +1,4 @@
+Here is text for PrP lab3.
+Hello git =3
+/* take some change
+just for lulz */
